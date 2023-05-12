48abfa13331fe7f2ff20dcff333a8a306cb2acca
GabLib2128386 <132417827+GabLib2128386@users.noreply.github.com>
2023-05-11 21:49:32 -0600

    Add files via upload

---
+++ Interfaces/src/com/pp2/App.java
@@ -0,0 +1,14 @@
+//Liberato Hernández Gabriel Alejandro ICO4
+package com.pp2;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Dog dog= new Dog();
+		dog.move();
+		dog.eat();
+
+	}
+
+}
