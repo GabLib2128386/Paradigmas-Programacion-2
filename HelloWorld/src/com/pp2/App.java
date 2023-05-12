70d946b179788e8a14e21773182deea9e9b03458
GabLib2128386 <132417827+GabLib2128386@users.noreply.github.com>
2023-05-11 21:00:30 -0600

    Add files via upload

---
+++ HelloWorld/src/com/pp2/App.java
@@ -0,0 +1,11 @@
+//Liberato Hernández Gabriel Alejandro ICO4
+package com.pp2;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
---
+++ HelloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author gonta
+ *
+ */
+module HelloWorld {
+}
